cebcfdb5e8a52526e41d872ec9f881f30a2df507
renanbmx123 <renanbmx123@gmail.com>
2021-04-05 13:03:00 -0300

    Refatorando maquina de estados.

--- main.cpp
+++ main.cpp
@@ -1,241 +1,141 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// #include "mbed.h"
-
-// #define WAIT_TIME_MS 500 
-// DigitalOut led1(LED1);
-
-// int main()
-// {
-//     printf("This is the bare metal blinky example running on Mbed OS %d.%d.%d.\n", MBED_MAJOR_VERSION, MBED_MINOR_VERSION, MBED_PATCH_VERSION);
-
-//     while (true)
-//     {
-//         led1 = !led1;
-//         thread_sleep_for(WAIT_TIME_MS);
-//     }
-// }
-
-
 #include "main.h"
+#include "FileBase.h"
+#include "FileHandle.h"
 #include <chrono>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <rt_sys.h>
 
-// Main function.
 int main(){
-  // Serial Configuration.
-  sleep_req = new DigitalOut(RADIO_SLEEP_REQ); // Xbee Sleep request pin
-  on_sleep = new DigitalIn(RADIO_ON_SLEEP);  //   Xbee on sleep pin.
-  uint8_t state;
-  char        gps_data;
-  char        data[200],  // Data to send
-  NorthSouth = 'F',       // North South direction
-  EastWest = 'F';         // East West direction
-  uint16_t    id;         // Node identification
-  uint8_t     hour;       //**********************
-  uint8_t     minute;     //**********************
-  uint8_t     second;     //**** Data and time UTC
-  uint8_t     day;        //**** ,from gps module.
-  uint8_t     month;      //**********************
-  uint16_t    year;       //**********************
-  float       g[3],       // Gyroscope
-  acc[3]                  // Accelerometer
-  ,mag[3],                // Magnetrometer
-  dht_t,                  // DHT22 temperature sensor
-  dht_h,                  // DHT22 humidity sensor
-  piezo,                  // Vibration sensor
-  vbat,                   // Batery percentage.
-  lat,                    // Latitude .
-  lon,                    // Longitude.
-  alt,                    // Altitude from gps module.
-  Balt,                   // Altitude from barometer sensor.
-  speed;                  // Speed over the ground
-  float      Bpress;      // Barometric pressure
-  // Instantiate objects.
-  DHT dht_th(p23,DHT22);                     // Dht (Digital humidity temperature) sensor on digital pin 23.
-  AnalogIn piezo_sensor(p20);                // Piezo sensor on analog pin 20.
-  MQ4 mq4(p18);                              // Gás sensor on analog pin 18.
-  BufferedSerial GPSSerial(p9, p10,9600);
-  Altimu lib_imu(p28,p27);
-  TinyGPSPlus tgps;
-  // Xbee object configuration.
-  RadioStatus radioStatus = xbee.init();
-  xbee.set_power_level(xbee_power_level);
+
+    sm.current_state = sm.current_state = ST_INIT;
+    while(1){
+
+    }
+}
+
+void st_init (){ 
+  // Initial configuration.
+  on_sleep     = new DigitalIn(RADIO_ON_SLEEP);  //   Xbee on sleep pin.
+  sleep_req    = new DigitalOut(RADIO_SLEEP_REQ); // Xbee Sleep request pin
+  sm.dht_th    = new DHT(p23,DHT22);                // Dht (Digital humidity temperature) sensor on digital pin 23.
+  sm.mq4       = new MQ4(p18);                              // Gás sensor on analog pin 18.
+  sm.GPSSerial = new BufferedSerial(p9, p10,9600);
+  sm.lib_imu   = new Altimu(p28,p27);
+  LocalFileSystem local("local");
+  sm.xbee_power_level = 0;
+  sm.xbee =new  XBeeLib::XBeeZB(RADIO_TX, RADIO_RX, RADIO_RESET, NC, NC, 9600);
+  XBeeLib::RadioStatus radioStatus = sm.xbee->init();
+  sm.xbee->set_power_level(sm.xbee_power_level);
+  sm.mcu_sleep = new bool(false);
 
   // Configure radio and get id number.
   uint32_t serialn_low;
   uint64_t current_panid;
   uint16_t current_channel_mask;
   // Get 2 bytes of the address module, set Node id with it.
-  xbee.get_param("SL", &serialn_low);
-  id = (uint16_t)serialn_low;
+  sm.xbee->get_param("SL", &serialn_low);
+  sm.id = (uint16_t)serialn_low;
   // Set PAN_ID if is not configured.
-  xbee.get_operating_panid(&current_panid);
+  sm.xbee->get_operating_panid(&current_panid);
   if (current_panid != NEW_PANID) {
-    xbee.set_panid(NEW_PANID);
+    sm.xbee->set_panid(NEW_PANID);
   }
   // Set CN (channel mask) if its is not set.
-  xbee.get_channel_mask(&current_channel_mask);
+  sm.xbee->get_channel_mask(&current_channel_mask);
   if (current_channel_mask != NEW_CHANNEL_MASK) {
-    xbee.set_channel_mask(NEW_CHANNEL_MASK);
-    xbee.write_config();
+    sm.xbee->set_channel_mask(NEW_CHANNEL_MASK);
+    sm.xbee->write_config();
   }
   mq4_heater = 1;         // Set heater on, for Mq4 sensor set up.
   GpsPwrPin = 1;          // Set gps on.
-  #if defined(DEBUG_LEDS)
-  led1 = 1;
-  #endif
-  //wait(30);              // Wait sensor warm up on cold start, first start.
   ThisThread::sleep_for(chrono::milliseconds(20*1000));
-  #if defined(DEBUG_LEDS)
-  led1 = 0;
-  #endif
-  MQ4_data_t MQ4_data;    // Store gas sensor information.
-  mq4.begin();            // Begin te R0 calculation and sensor calibration.
-  // Verify network connection at first start.
-  XbeeNetworkSearch();
-  state = READ_SEND_DATA_STATE;
+  sm.mq4->begin();        // Begin te R0 calculation and sensor calibration.
+  sm.current_state = ST_NET; // Go to next state.
+}
 
-  while(1){
-
-    switch (state) {
-      case READ_SEND_DATA_STATE:
-      //Read data
-        for (int j = 0; j < 10; j++){
-        // Read 3 axis gyroscope sensor.
-        lib_imu.read_L3GD20(&g[0],&g[1],&g[2]);
-        // Read linear accleration and magnecti field.
-        lib_imu.read_LSM303D(&acc[0],&acc[1],&acc[2],&mag[0],&mag[1],&mag[2]);
-        // Read imu pressure in hPa and Altitude in metters.
-        lib_imu.read_LPS25H(&Bpress, &Balt);
-        // Read piezoeletric sensor.
-        piezo = piezo_sensor.read();
-        // Read 9V baterry charge.
-        vbat = bat_level();
-        dht_th.readData();
-        // Read DHT22 temperature in celcius.
-        dht_t = dht_th.ReadTemperature(CELCIUS);
-        // Read DHT22 relative humidity
-        dht_h = dht_th.ReadHumidity();
-        // Read MQ4 gas sensor
-        mq4.read(&MQ4_data);
-        // Reading GPS data...
-        for(int i =0;i<100;i++){
-          if(GPSSerial.readable()){
-            //while (!tgps.encode(GPSSerial.getc()));
-            GPSSerial.read(&gps_data ,1);
-            while (!tgps.encode(gps_data));
-            i = 100;
-          }
-        }
-        if(tgps.location.isValid()){
-          lat = tgps.location.lat();
-          lon = tgps.location.lng();
-        }else{
-          lat = 0;
-          lon = 0;
-        }
-        if(tgps.altitude.isValid()){
-          alt = tgps.altitude.meters();
-        }else{
-          alt = 0;
-        }
-        if(tgps.speed.isValid()){
-          speed = tgps.speed.mps();
-        }else{
-          speed = 0;
-        }
-        if((tgps.NorthSouth == 'S') |
-           (tgps.NorthSouth =='N')){
-          NorthSouth = tgps.NorthSouth;
-        }
-        else{
-          NorthSouth = 'F';
-        }
-        if ((tgps.EastWest == 'W') |
-            (tgps.EastWest == 'E')){
-          EastWest = tgps.EastWest;
-        }else{
-          EastWest = 'F';
-        }
-        if (tgps.time.isValid())
-        {
-          //second[1] = (tgps.time.second()%10)+48;
-          //second[0] = (tgps.time.second()/10) +48;
-          //minute[1] = (tgps.time.minute() %10)+48;
-          second = tgps.time.second();//minute[0] = (tgps.time.minute()/10) +48;
-          minute = tgps.time.minute();//hour[1] = (tgps.time.hour()%10)+48;
-          hour   = tgps.time.hour();//hour[0] = (tgps.time.hour()/10) +48;
-          day    = tgps.date.day();
-          year   = tgps.date.year();
-          month  = tgps.date.month();
-          
-        }
-        //Store data into a vector.
-        sprintf(data,"%4x,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.2f,%.2f,%.2f,%.2f,%02d%02d%02d,%02d%02d%4d,%.3f,%.3f,%.2f,%c,%c,%.2f,%.2f,%.2f,%d",id,
-        g[0], g[1], g[2],acc[0],acc[1], acc[2], mag[0], mag[1], mag[2],Bpress, dht_h,dht_t,MQ4_data.ch4,piezo, hour, minute,second,year,month,day, lon, lat, speed, NorthSouth, EastWest, Balt, alt,vbat,xbee_power_level);
-        #if defined(WRITE_LOCAL_DATA)
-        file_write(data);
-        #endif        
-        while(send_data_to_coordinator(xbee, data) != TxStatusSuccess) // try to send data, every 100 ms
-        {
-          #if defined(DEBUG_LEDS)
-          led1 = !led1;
-          #endif
-          //wait_ms(500);
-          ThisThread::sleep_for(chrono::milliseconds(300));
-        }
-        //wait_ms(800);
-        ThisThread::sleep_for(chrono::milliseconds(500));
+void st_net (){
+  // whaiting for xbee join to network.
+  while (!sm.xbee->is_joined()) {
+    sm.xbee->set_power_level(sm.xbee_power_level);
+    ThisThread::sleep_for(std::chrono::seconds(1));
+    sm.conn_retries ++;
+    if(sm.conn_retries == SEARCH_NETWORK_RETRIES){
+      if(sm.xbee_power_level < 4){
+        sm.xbee_power_level ++;
       }
-        state = SLEEP_STATE;
-      break;
-      case SLEEP_STATE:
-        //Debug Led
-        #if defined(DEBUG_LEDS)
-        led2 = !led2;
-        #endif
+        else{
+            // sleep_radio();                      // sleep radio
+            // tim.attach(&sleepManager,std::chrono::seconds(TIMEOUT));  // Call this function every time when reach TIMEOUT value.
+            // sleep();                            // Sleep for a while when network is not reach.
+            // awake_radio();                      // wake up radio
+            sm.conn_retries = 0;                   // Reset the count variable.
+            sm.current_state = ST_SLEEP;
 
-        sleep_radio();          // Set radio to sleep.
-        mq4_heater = 0;         // Disable Mq4 sensor heater resistence.
-        GpsPwrPin = 0;
-        tim.attach(&sleepManager,std::chrono::seconds(TIMEOUT));  // attach timeout function
-        sleep();
-        //Debug Led
-        #if defined(DEBUG_LEDS)
-        led2 = !led2;
-        #endif
-
-        mq4_heater = 1;           // Disable Mq4 sensor heater resistence.
-        GpsPwrPin = 1;            // Enable gps module.
-        awake_radio();
-        //wait(30);
-        state = SEARCH_NETWORK_STATE;
-        // whaiting for xbee join to network.
-      break;
-      case SEARCH_NETWORK_STATE:    // Looking for network before
-        XbeeNetworkSearch();
-        state = READ_SEND_DATA_STATE;
-      break;
+        }
+    }else {
+        sm.current_state = ST_SDATA;
     }
-
   }
 }
 
-// Auxliar Function.
-void file_write(char* data_file){
-    
-    fp = fopen("/local/out.csv", "a");
-    if(fp == NULL){
+void st_rdata(){
+    //Read data
+    // Read 3 axis gyroscope sensor.
+    sm.lib_imu->read_L3GD20(&sm.g[0],&sm.g[1],&sm.g[2]);
+    // Read linear accleration and magnecti field.
+    sm.lib_imu->read_LSM303D(&sm.acc[0],&sm.acc[1],&sm.acc[2],&sm.mag[0],&sm.mag[1],&sm.mag[2]);
+    // Read imu pressure in hPa and Altitude in metters.
+    sm.lib_imu->read_LPS25H(&sm.Bpress, &sm.Balt);
+    sm.dht_th->readData();
+    sm.mq4->read(&sm.MQ4_data);
+    // Reading GPS data...
+    for(int i =0;i<100;i++){
+      if(sm.GPSSerial->readable()){
+      sm.GPSSerial->read(&sm.gps_data ,1);
+        while (!sm.tgps.encode(sm.gps_data));
+        i = 100;
+            }
+        }
+        int id;
+        char data;
+        sprintf(data, "asd %d",id);
+        //sprintf(sm.data, "%d", sm.id);
+        //Store data into a vector.
+        // sprintf(sm.data,"%4x,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.2f,%.2f,%.2f,%.2f,%02d%02d%02d,%02d%02d%4d,%.3f,%.3f,%.2f,%c,%c,%.2f,%.2f,%.2f,%d",sm.id,
+        // sm.g[0], sm.g[1], sm.g[2],sm.acc[0],sm.acc[1], sm.acc[2], sm.mag[0], sm.mag[1], sm.mag[2],sm.Bpress, sm.dht_h,sm.dht_t,sm.MQ4_data.ch4,piezo_sensor.read(), sm.tgps.time.hour(),
+        // sm.tgps.time.minute(),sm.tgps.time.second(),sm.tgps.date.year(),sm.tgps.date.month(),sm.tgps.date.day(), sm.tgps.location.lng(), sm.tgps.location.lat(), sm.tgps.speed.mps(), 
+        // sm.tgps.NorthSouth, sm.tgps.EastWest, sm.Balt, sm.tgps.altitude.meters(),bat_level(),sm.xbee_power_level);
         
-        error("Arquivo nao encontrado\n\r");
+        #if defined(WRITE_LOCAL_DATA)
+        file_write(data);
+        #endif        
+        sm.current_state = ST_SDATA;
+
+}
+void st_sdata(){
+    const XBeeLib::TxStatus txStatus = sm.xbee->send_data_to_coordinator(sm.data, sizeof(sm.data));
+    uint8_t retries = SEND_RETRIES;
+    while(txStatus != XBeeLib::TxStatusSuccess) {
+        ThisThread::sleep_for(chrono::milliseconds(300));
+        if((--retries) == 0){
+           sm.current_state = ST_NET;
+           return;
+            }
     }
-    fprintf(fp,"%s\n",data_file);
-    fclose(fp);
+        sm.current_state = ST_SLEEP;
 }
 
+
+void st_sleep(){
+     sleep_radio();                      // sleep radio
+     sm.tim->attach(&sleepManager,std::chrono::seconds(TIMEOUT));  // Call this function every time when reach TIMEOUT value.
+     sleep();                            // Sleep for a while when network is not reach.
+     awake_radio();                      // wake up radio
+}
+
+// Auxliar Function.
 bool is_radio_sleeping()            // Set radio to sleep, return 0 if radio sleep.
 {
   assert(on_sleep != NULL);
@@ -258,44 +158,18 @@ void awake_radio()                // Wake up xbee radio funcion.
 
 void sleepManager()              // Sleep Manager function.
 {
-  tim.detach();   // Deatach timer interrupt.
-  #if defined(DEBUG_LEDS)
-  led4 = !led4;
-  #endif
-}
-TxStatus send_data_to_coordinator(XBeeZB& xbee, char *data)
-{
-  const TxStatus txStatus = xbee.send_data_to_coordinator((const uint8_t *)data, strlen(data));
-  return txStatus;
+  sm.tim->detach();   // Deatach timer interrupt.
+  sm.current_state = ST_NET;
 }
 
-float bat_level(){
-  return bat_pin.read()*5;
-}
-
-void XbeeNetworkSearch(){
-  // whaiting for xbee join to network.
-  while (!xbee.is_joined()) {
-    xbee.set_power_level(xbee_power_level);
-    //wait_ms(1000);    
-    ThisThread::sleep_for(std::chrono::seconds(1));
-    conn_retries ++;
-    if(conn_retries == SEARCH_NETWORK_RETRIES){
-      if(xbee_power_level < 4)
-      {
-        xbee_power_level ++;
-      }
-        else{
-            
-            sleep_radio();                      // sleep radio
-            tim.attach(&sleepManager,std::chrono::seconds(TIMEOUT));  // Call this function every time when reach TIMEOUT value.
-            sleep();                            // Sleep for a while when network is not reach.
-            awake_radio();                      // wake up radio
-            conn_retries = 0;                   // Reset the count variable.
-        }
+void file_write(char* data_file){
+    
+    sm.fp = fopen("/local/out.csv", "a");
+    if(sm.fp == NULL){
+        
+        error("Arquivo nao encontrado\n\r");
     }
-    #if defined(DEBUG_LEDS)
-    led3 = !led3;
-    #endif
-  }
+    fprintf(sm.fp,"%s\n",data_file);
+    fclose(sm.fp);
 }
+
--- main.h
+++ main.h
@@ -8,12 +8,15 @@
 #include "MQ4.h"
 #include "DHT.h"
 #include "XBeeLib.h"
+#include <cstdint>
 
 // Defines
 #define TIMEOUT                50    // Set ticker time, for time interrupt.
 #define NEW_PANID             0x1234 // Network Pan ID.
 #define NEW_CHANNEL_MASK      0x7FFF // Network Channel.
 #define SEARCH_NETWORK_RETRIES  20   // Numbers of retries search network.
+#define DATA_SIZE               200  // Byte numbers of data size.
+#define SEND_RETRIES            5
 
 //#define WRITE_LOCAL_DATA             // For write local data on the board usb storage.
 //#define DEBUG_LEDS                 // USED FOR DEBUG.
@@ -23,40 +26,78 @@
 #define SLEEP_STATE             2    // Sleep state, shutdown all modules and sensor then go to sleep.
 #define SEARCH_NETWORK_STATE    1    // Search for network, stays on this state if network doenst show up.
 #define READ_SEND_DATA_STATE    0    // This state send data after reading it.
+/* New state machine*/
+/* Estados:  https://www.inf.pucrs.br/~emoreno/undergraduate/SI/orgarq/class_files/Aula07.pdf
+0 - Estado inicial, apenas configura os modulos e realiza a calibragem de alguns sensores alem de sua inicialização, 
+ales de configurar os parametros da rede, como PANID e a potência do môdulo.
+1 - Estado que verifica se o modulo Xbee estabeleceu conexão na rede, caso sim vai para o proximo estágio senão apos 
+N tentativas entra no modo sleep
+2 - Estado que realiza a leitura de todos os sensores, armazenando os dados em um buffer separados por virgula.
+3 - Estado que realiza a tentativa de envio dos dados, caso não consiga apos N tentativas, volta pro estado de verificação da rede.
+4 - Estado sleep, aciona o pina de PWR_CTRL que coloca em modo low power, acina o pino de sleep da Xbee, e coloca a controladora
+em modo sleep. Fica neste modo por um periodo determiando de tempo T, que pode variar conforme a politica estabelecida. 
+*/
+typedef enum {
+    ST_INIT = 0, ST_NET, ST_RDATA, ST_SDATA, ST_SLEEP
+} States;
+  
+struct StateMachine {
+    States current_state, previous_state;
+    MQ4*        mq4;                              /*Gás sensor on analog pin 18.*/
+    DHT*        dht_th;                     /*Dht (Digital humidity temperature) sensor on digital pin 23.*/
+    Altimu*     lib_imu;
+
+    char        gps_data, NorthSouth = 'F', EastWest =  'F';
+    uint8_t     hms[3], day_month[2], xbee_power_level = 0;
+    uint8_t     data[DATA_SIZE]; //char data[200],  /*Data to send*/
+    uint16_t    id, year;
+    float       g[3]/*Gyroscope*/,acc[3]/*Accelerometer*/,mag[3]/*Magnetrometer*/,dht_t/*DHT22 temperature sensor*/,
+                dht_h/*DHT22 humidity sensor*/, piezo/*Vibration sensor*/,vbat/*Batery percentage*/,
+                lat/*Latitude*/,lon/*Longitude*/,alt/*Altitude from gps module*/,Balt/*Altitude from barometer sensor*/,
+                speed/*Speed over the ground*/,Bpress;/*Barometric pressure*/
+    TinyGPSPlus tgps;        
+    MQ4_data_t MQ4_data;    // Store gas sensor information.
+
+    BufferedSerial* GPSSerial;
+    XBeeLib::XBeeZB*     xbee;
+    bool    *mcu_sleep;   // microcontroler sleep control.
+    Ticker  *tim; // Ticker, for timer interrupt . 
+    int conn_retries = 0; // Count numbers of trying to connect 
+    FILE *fp;   // Logar dados no localfile da placa.
+    
+
+} sm;
+
+void state_machine();
+
+void st_init ();
+void st_net ();
+void st_rdata();
+void st_sdata();
+void st_sleep();
+
+
+
+/*----------------------------------------------------------------------------------------------------------------------------------------
+* ----------------------------------------------------------------------------------------------------------------------------------------
+*/
 
 
 // Global objects.
-using namespace XBeeLib;      // Namespace for XbeeLib
+//using namespace XBeeLib;      // Namespace for XbeeLib
 DigitalOut* sleep_req = NULL; //Xbee sleep request pin.
-DigitalIn* on_sleep = NULL;   // Xbee sleep read pin, for check sleep state.
-AnalogIn bat_pin(p16);        // Batery level read analog pin 16.
-DigitalOut mq4_heater(p17);   // MQ4 heater power pin control.
-DigitalOut GpsPwrPin(p15);    // GPS power pin control`
-// This is used for debug, these leds are built in leds.
-#if defined(DEBUG_LEDS)
-DigitalOut led1(LED1);        // Debug Led 1
-DigitalOut led2(LED2);        // Debug Led 1
-DigitalOut led3(LED3);        // Debug Led 1
-DigitalOut led4(LED4);        // Debug Led 1
-#endif
-// Control variables
-uint8_t xbee_power_level = 0; //Xbee radio power level, set to minumun
-bool mcu_sleep = false;   // microcontroler sleep control.
-Ticker tim; // Ticker, for timer interrupt .
-int conn_retries = 0; // Count numbers of trying to connect
-LocalFileSystem local("local");
-FILE *fp;   // Logar dados no localfile da placa.
-// ---------------------------------
+DigitalIn*  on_sleep = NULL;   // Xbee sleep read pin, for check sleep state.
+AnalogIn    bat_pin(p16);        // Batery level read analog pin 16.
+DigitalOut  mq4_heater(p17);   // MQ4 heater power pin control.
+DigitalOut  GpsPwrPin(p15);    // GPS power pin control`
+AnalogIn piezo_sensor(p20);                // Piezo sensor on analog pin 20.
 
-XBeeZB xbee = XBeeZB(RADIO_TX, RADIO_RX, RADIO_RESET, NC, NC, 9600);
-//Serial  pc(USBTX, USBRX,9600);// Serial Debug.
-// Auxliar Function.
+
 float bat_level();
 bool is_radio_sleeping();
 void sleep_radio();
 void awake_radio();
 void sleepManager();
-TxStatus send_data_to_coordinator(XBeeZB& xbee, char *data);
 float bat_level();
 void XbeeNetworkSearch();
 void file_write(char* data_file);
